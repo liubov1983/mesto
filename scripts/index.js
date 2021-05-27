9df16f8c630c3e445edb73103abd1616ed735a54
Liubov Yakovleva <regnum1983@yandex.ru>
2021-05-27 19:50:41 +0300

    восстановление проекта

--- scripts/index.js
+++ scripts/index.js
@@ -5,21 +5,31 @@ let formElement = popup.querySelector('.form');
 let nameInput = formElement.querySelector('.form__item_el_name');
 let jobInput = formElement.querySelector('.form__item_el_job');
 let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__descr');
+let profileJob = document.querySelector('.profile__job');
 
-editButton.addEventListener('click', function () {
+function openPopup() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
   popup.classList.add('popup_opened');
-})
+}
 
-closeButton.addEventListener('click', function () {
+function closePopup() {
   popup.classList.remove('popup_opened');
-})
+  nameInput.value = '';
+  jobInput.value = '';
+}
 
 function formSubmitHadler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popup.classList.remove('popup_opened');
+  closePopup();
 }
 
+editButton.addEventListener('click', openPopup);
+
+closeButton.addEventListener('click', closePopup);
+
 formElement.addEventListener('submit', formSubmitHadler);
+
+
